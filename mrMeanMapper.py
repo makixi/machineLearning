f877e7831269c2d8ef40eb32c21321b67027bebb
makixi <472259345@qq.com>
2018-12-12 21:31:45 +0800

    submit

---
+++ mrMeanMapper.py
@@ -0,0 +1,15 @@
+import sys
+from numpy import mat,mean,power
+
+### 分布式均值和方差计算的mapper
+
+def read_input(file):
+	for line in file:
+		yield line.rstrip()
+
+input=read_input(sys.stdin)
+input=[float(line) for line in input]
+numInputs=len(input)
+input=mat(input)
+sqInput=power(input,2)
+print("%d\t%f\t%f"%(numInputs,mean(input),mean(sqInput)))
